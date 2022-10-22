ebf7de689fd5004c5352ed43bffbbc2c91ef4b51
xTripleAx <aliatieh77@gmail.com>
2022-10-22 15:48:59 +0300

    Added the cipher code

---
+++ SubstitutionCipherGenerator.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class SubstitutionCipherGenerator {
+
+	public static void main(String[] args) {
+		
+		char[] alphabet = new char[27];
+		char[] Substitution = new char[27]; 
+		Random rand = new Random();
+
+		// Initialize the alphabet 
+		for (int i = 0; i < 26; i++)
+		{
+			alphabet[i] = (char) ((int)'A' + i) ; 
+			Substitution[i] = alphabet[i]; 
+		}
+		alphabet[26] = ' ';
+		Substitution[26] = alphabet[26]; 
+
+		
+		// Create the substitution Cipher bu shuffling the Substitution Array three times
+		for (int j = 0; j < 3 ; j++)
+		{
+			for (int i = 0; i < alphabet.length; i++) {
+			int randomIndexToSwap = rand.nextInt(Substitution.length);
+			char temp = Substitution[randomIndexToSwap];
+			Substitution[randomIndexToSwap] = Substitution[i];
+			Substitution[i] = temp;
+		}
+		} 
+		
+		System.out.println(Arrays.toString(alphabet));
+		System.out.println(Arrays.toString(Substitution));
+
+	}
+}
